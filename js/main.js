3a786df277051f30bf690109035c048d7a0d9611
abdelrhman-mohamed-morsy <abdelrhmanmorsy199@gmail.com>
2025-08-19 14:01:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,84 @@
+// let age = 18 ;
+// let gender ='male' ;
+
+// if(age>=18 && gender == 'male' ) {
+//     console.log('halow');
+    
+// }
+
+// else{
+//     console.log("no now");
+    
+// }
+
+// let cuntre="egypt" ;
+// let price=100;
+
+// if( cuntre=="egypy" && cuntre=="syria"){
+//     price-=20
+// }
+
+// else if ( cuntre=="ksa" &&cuntre=="ksa"){
+//     price-=10
+// }
+// console.log(price);
+
+    //switch
+
+// let day=1;
+
+// switch (day){
+// case 1 :
+// day ="sun day";
+// break;
+
+// case 2:
+// day ="manday";
+// break;
+
+// case 3:
+// day ="tuseday";
+// break;
+
+// case 4:
+// day ="wensday";
+// break;
+
+// case 5:
+// day ="thrisday";
+// break;
+
+// case 6:
+// day ="friday";
+// break;
+
+// case 7:
+// day ="saterday";
+// break;
+
+// }
+// console.log(`that day is ${day}`);
+     
+
+    //while loop
+// let i=1;
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+
+      //for loop
+// for(let i=0; i<=100;i+=2){
+//     console.log(i);
+// }
+
+
+
+let num= 15;
+if(num%2 == 0){
+    console.log(`${num} is even`)
+}
+else{
+    console.log(`${num} is odd`)
+}
